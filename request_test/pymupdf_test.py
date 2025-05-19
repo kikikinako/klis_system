43e0dab5598c0af2a684018c2b5c34a67b5e49ca
romanohu <shi301649@gmail.com>
2025-05-19 18:26:03 +0900

    5/19の作業

--- request_test/pymupdf_test.py
+++ request_test/pymupdf_test.py
@@ -1,9 +1,16 @@
 import fitz  # PyMuPDF
 import re
+from janome.tokenizer import Tokenizer
+import os 
 
 index = {}
+t = Tokenizer()
 
-for number in range(212,388):
+pattern = re.compile(r"[　-ー]$")
+num_pattern = re.compile(r"[^\u3040-\u30FF\u4E00-\u9FFFa-zA-Z]")
+
+
+for number in range(300,388):
     # PDFファイルを開く
     doc = fitz.open(f"pdfs/{number}.pdf")
     # 全ページのテキストを結合
@@ -13,6 +20,21 @@ for number in range(212,388):
     doc.close()
     # 空白・改行・タブを削除（日本語のように単語区切りが不要な言語に適応）
     clean_text = all_text.replace(" ", "").replace("\n", "").replace("\t", "")
+    
+    tokens = t.tokenize(clean_text)
+    
+    for token in tokens:
+        if pattern.match(token.surface):
+            continue
+        if num_pattern.match(token.surface):
+            continue
+        if token.surface in index:
+            if number not in index[token.surface]:
+                index[token.surface].append(number)
+        else:
+            index[token.surface] = [number]
+        
+print(index)
     
     
     
